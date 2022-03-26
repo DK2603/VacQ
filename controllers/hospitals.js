7d76d286718fa1ccd11cf1ea68d053fb274e61e6
DK2603 <Dusida.khun@gmail.com>
2022-03-26 16:02:36 +0700

    VacCenters

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -2,14 +2,101 @@ const express = require('express');
 const router = express.Router();
 const app=express();
 const Hospital = require('../models/Hospital.js');
+const vacCenter = require('../models/VacCenter')
+
+//@desc Get vaccine centers
+//@route GET /api/v1/hospitals/vacCenters/
+//@access Public
+exports.getVacCenters = (req,res,next) => {
+    vacCenter.getAll((err,data) => {
+        if(err)
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while retrieving Vaccine Centers."
+        });
+        else res.send(data);
+    });
+};
+
+
+
+
+
 
 //@desc     Get all hospitals
 //@route    GET /api/v1/hospitals
 //@access   public
 exports.getHospitals= async (req,res,next)=>{
-    try{
-        const hospitals = await Hospital.find();
-        res.status(200).json({success:true, count: hospitals.length, data: hospitals});
+    let query;
+
+
+    //Copy req.query
+    const reqQuery={...req.query};
+
+    //Fields to exculde
+    const  removeFields=['select','sort','page','limit'];
+
+    //Loop over remove fields and delete them from reqQuery
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+
+    //Create query string
+    let queryStr=JSON.stringify(reqQuery);
+    queryStr=queryStr.replace(/\b(gt| gte|lt|lte|in)\b/g, match=> `$${match}`);
+   
+    //finging resource
+    query=Hospital.find(JSON.parse(queryStr)).populate('appointments');
+
+
+    //Select Fields
+    if(req.query.select){
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
+    }
+    //Sort
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortBy);
+    } else{
+        query=query.sort('-createdAt');
+    }
+
+    //pagination
+    const page=parseInt(req.query.page,10)|| 1;
+    const limit=parseInt(req.query.limit,10) || 25;
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+    
+
+    
+
+    try{ 
+        const total=await Hospital.countDocuments();
+        query=query.skip(startIndex).limit(limit);
+        //Executing query
+        const hospitals = await query;
+        //pagination result
+        const pagination ={};
+
+        if(endIndex<total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+
+        if(startIndex>0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+
+        res.status(200).json({
+            success:true, 
+            count: hospitals.length, 
+            data: hospitals
+        });
     } catch(err){
         res.status(400).json({sucess:false});
     }
@@ -69,10 +156,11 @@ exports.updateHospital= async (req,res,next)=>{
 exports.deleteHospital= async (req,res,next)=>{
     console.log(req.body);
     try{
-        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        const hospital = await Hospital.findById(req.params.id);
         if(!hospital){
             return res.status(400).json({success:false});
         }
+        hospital.remove();
         res.status(200).json({success:true,data:{}});
     }
     catch(err){
