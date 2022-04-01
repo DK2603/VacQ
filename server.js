216c7290a9b6a67029b3f7ca1e8b174def491144
DK2603 <Dusida.khun@gmail.com>
2022-04-02 01:07:45 +0700

    VacCenters

--- server.js
+++ server.js
@@ -14,8 +14,16 @@ connectDB();
 const hospitals =require('./routes/hospitals');
 const auth = require('./routes/auth');
 const appointments =require('./routes/appointments');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss=require('xss-clean');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+const cors=require('cors');
 
 const app=express();
+
+
 
 //cookie parser
 app.use(cookieParser());
@@ -23,12 +31,38 @@ app.use(cookieParser());
 //Body parser
 app.use(express.json());
 
+
+// Sanitize dara
+app.use(mongoSanitize());
+
+//Set security header 
+app.use(helmet());
+
+//Prevent XSS create
+app.use(xss());
+
+//Rate Limiting
+const limiter= rateLimit({
+    windowsMS:10*60*1000, //10 mins
+    max: 1
+});
+app.use(limiter);
+
+//Prevent http param polutions
+app.use(hpp());
+
+
+// Enable CORS
+app.use(cors());
+
+
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth',auth);
 app.use('/api/v1/appointments', appointments);
 const PORT=process.env.PORT || 5000;
 const server =app.listen(PORT,console.log('Server running in', process.env.NODE_ENV,'mode on port', PORT));
+
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
